7d49c808e05f0c816829e2c8f3560174210217e6
Conner Ward <conner.ward@mercedes-benz.com>
2026-03-11 21:18:48 -0700

    Revert "Add LayerPack node to bundle images into layers input for SaveLayeredTIFFXMP"
    This reverts commit 608db3ff71717ddf838c8690cd882a7e064d6c47.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,13 @@
-from .nodes import SaveImageXMP, SaveLayeredTIFFXMP, LayerPack
+from .nodes import SaveImageXMP, SaveLayeredTIFFXMP
 
 NODE_CLASS_MAPPINGS = {
     "SaveImageXMP": SaveImageXMP,
     "SaveLayeredTIFFXMP": SaveLayeredTIFFXMP,
-    "LayerPack": LayerPack,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "SaveImageXMP": "Save Image (XMP)",
     "SaveLayeredTIFFXMP": "Save Layered TIFF (XMP)",
-    "LayerPack": "Layer Pack",
 }
 
 WEB_DIRECTORY = "./web"
--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,3 +1,2 @@
 from .save_image_xmp import SaveImageXMP
 from .save_layered_tiff_xmp import SaveLayeredTIFFXMP
-from .layer_pack import LayerPack
